6f20f139007236bc6d5d114906702264bb2de9af
Alifiyah Shahid <shahid.alifiyah@gmail.com>
2024-08-27 16:21:26 -0400

    Sharing State with react Context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import ToDoListWithToolbar from "./components/todo/ToDoListWithToolbar";
 import { TodosDataProvider } from "./contexts/ToDosDataContext";
 import ToDoManager from "./components/todo/ToDoManager";
 import Layout from "./components/layout/Layout";
+
+export const ThemeContext = createContext({});
 
 const App = () => {
   const [displayStatus, setDisplayStatus] = useState("all"); // all, pending, completed
@@ -10,20 +12,32 @@ const App = () => {
   const [searchText, setSearchText] = useState("");
   const [darkTheme, setDarkTheme] = useState(false);
   const toggleTheme = () => setDarkTheme(!darkTheme);
+
+  const value = {
+    darkTheme: darkTheme,
+    toggleTheme: toggleTheme,
+  };
   return (
     <TodosDataProvider>
-      <Layout toggleTheme={toggleTheme} darkTheme={darkTheme}>
-        <ToDoListWithToolbar
-          displayStatus={displayStatus} setDisplayStatus={setDisplayStatus}
-          import={important} setImportant={setImportant}
-          searchText={searchText} setSearchText={setSearchText}
-        >
-          <ToDoManager
-            displayStatus={displayStatus} important={important}
-            searchText={searchText} darkTheme={darkTheme}
-          />
-        </ToDoListWithToolbar>
-      </Layout>
+      <ThemeContext.Provider value={{ darkTheme, toggleTheme }}>
+        <Layout toggleTheme={toggleTheme} darkTheme={darkTheme}>
+          <ToDoListWithToolbar
+            displayStatus={displayStatus}
+            setDisplayStatus={setDisplayStatus}
+            import={important}
+            setImportant={setImportant}
+            searchText={searchText}
+            setSearchText={setSearchText}
+          >
+            <ToDoManager
+              displayStatus={displayStatus}
+              important={important}
+              searchText={searchText}
+              // darkTheme={darkTheme} Removed this because it is used easily using themeContext
+            />
+          </ToDoListWithToolbar>
+        </Layout>
+      </ThemeContext.Provider>
     </TodosDataProvider>
   );
 };
